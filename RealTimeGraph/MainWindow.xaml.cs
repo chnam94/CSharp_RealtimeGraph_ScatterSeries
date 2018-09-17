57e732fbcf364a784b3ae4cd5dc7f427e69afc41
chunghyeon0920 <nch2078@daum.net>
2018-09-17 11:14:26 +0900

    Add projects

---
+++ RealTimeGraph/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace RealTimeGraph
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            testGraph.Series = new SeriesCollection
+            {
+                new ScatterSeries
+                {
+                    Values = new ChartValues<ScatterPoint>
+                    {
+                        new ScatterPoint(5, 5, 20),
+                        new ScatterPoint(3, 4, 80),
+                        new ScatterPoint(7, 2, 20),
+                        new ScatterPoint(2, 6, 60),
+                        new ScatterPoint(8, 2, 70)
+                    },
+                    MinPointShapeDiameter = 15,
+                    MaxPointShapeDiameter = 45,
+                    
+                },
+                new ScatterSeries
+                {
+                    Values = new ChartValues<ScatterPoint>
+                    {
+                        new ScatterPoint(7, 5, 1),
+                        new ScatterPoint(2, 2, 1),
+                        new ScatterPoint(1, 1, 1),
+                        new ScatterPoint(6, 3, 1),
+                        new ScatterPoint(8, 8, 1)
+                    },
+                    PointGeometry = DefaultGeometries.Triangle,
+                    MinPointShapeDiameter = 15,
+                    MaxPointShapeDiameter = 45
+                    
+                },
+                new ScatterSeries
+                {
+                    Values = new ChartValues<ScatterPoint>
+                    {
+                        new ScatterPoint(7, 5, 1),
+                        new ScatterPoint(2, 2, 1),
+                        new ScatterPoint(1, 1, 1),
+                        new ScatterPoint(6, 3, 1),
+                        new ScatterPoint(8, 8, 1)
+                    },
+                    PointGeometry = DefaultGeometries.Triangle,
+                    MinPointShapeDiameter = 15,
+                    MaxPointShapeDiameter = 45
+
+                }
+            };
+            testGraph.AxisX.Add(new Axis
+            {
+                MaxValue = 15
+            });
+            testGraph.AxisY.Add(new Axis
+            {
+                MaxValue = 10
+            });
+            
+
+            DataContext = this;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(UpdateAllOnClick);
+            timer.Start();
+        }
+        public SeriesCollection SeriesCollection { get; set; }
+
+        private void UpdateAllOnClick(object sender, EventArgs e)
+        {
+            var r = new Random();
+            foreach (var series in testGraph.Series)
+            {
+                
+                foreach (var bubble in series.Values.Cast<ScatterPoint>())
+                {
+                    bubble.X = r.NextDouble() * 10;
+                    bubble.Y = r.NextDouble() * 10;
+                    bubble.Weight = r.NextDouble() * 10;
+                }
+            }
+        }
+    }
+}
